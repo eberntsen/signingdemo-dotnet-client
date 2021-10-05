3db0a9d3e82d782f63211dae1511453ee82f3c04
Eirik Berntsen <eirikb@uc.no>
2021-10-05 10:58:38 +0200

    Remove completed

--- src/Console/SampleRSA.cs
+++ src/Console/SampleRSA.cs
@@ -21,56 +21,7 @@ namespace Console
     {
         public static async Task Run(string[] args)
         {
-            using (var serviceProvider = new ServiceCollection().Configure(ConfigureServices).BuildServiceProvider())
-            {
-                using (var signerFactory = serviceProvider.GetRequiredService<IRequestSignerFactory>())
-                {
-                    var logger = serviceProvider.GetService<ILogger<SampleRSA>>();
-                    var signedRequestForRSA = await SampleSignRSA(signerFactory);
-
-                    using (var httpClient = new HttpClient())
-                    {
-                        var response = await httpClient.SendAsync(signedRequestForRSA);
-                        if (response.IsSuccessStatusCode)
-                        {
-                            logger?.LogInformation("{0} - GET request response: {1}", response.StatusCode, response.StatusCode);
-                        }
-                        else
-                        {
-                            logger?.LogError("{0} - GET request response: {1}, {2}", response.StatusCode, response.ReasonPhrase, response.Content.ToString());
-                        }
-                        var responseContentTask = response.Content?.ReadAsStringAsync();
-                        var responseContent = responseContentTask == null ? null : await responseContentTask;
-                        if (responseContent != null) logger?.LogInformation(responseContent);
-                    }
-                }
-            }
-        }
-
-        public static void ConfigureServices(IServiceCollection services)
-        {
-            var cert = new X509Certificate2(File.ReadAllBytes("./signing_demo.p12"), "pw", X509KeyStorageFlags.Exportable);
-
-            services
-                .AddLogging(configure => configure.AddConsole().SetMinimumLevel(LogLevel.Debug))
-                .AddHttpMessageSigning()
-                .UseKeyId("signing-demo-v1")
-                .UseSignatureAlgorithm(SignatureAlgorithm.CreateForSigning(cert));
-        }
-
-        private static async Task<HttpRequestMessage> SampleSignRSA(IRequestSignerFactory requestSignerFactory)
-        {
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri("https://signingtest.azurewebsites.net/api/stuff"),
-                Method = HttpMethod.Get,
-                Content = new StringContent("", Encoding.ASCII, "application/json")
-            };
-
-            var requestSigner = requestSignerFactory.CreateFor("signing-demo-v1");
-            await requestSigner.Sign(request);
-
-            return request;
-        }
+            
+        } 
     }
 }
